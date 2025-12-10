c59b2bd62213324c15ef5e4c708471f3533b876f
Praveen Boppana <praveen@prinics.com>
2025-12-10 11:16:33 +0530

    save to file and load from file

--- index.js
+++ index.js
@@ -1,46 +1,66 @@
-    const express = require('express');
-    const app = express();
-    
-    var port = process.env.PORT || 3000;
+const fs = require('fs');
+const ini = require('ini');
+const express = require('express');
+const app = express();
 
-    app.use(express.json());
+var port = process.env.PORT || 3000;
 
-    let printers = [
-        { id: 1, enabled: true, ribbon: 40, paper: 20 },
-        { id: 2, enabled: true, ribbon: 30, paper: 15 },
-        { id: 3, enabled: true, ribbon: 20, paper: 5 },
-        { id: 4, enabled: true, ribbon: 10, paper: 10 },
-    ]
+app.use(express.json());
 
-    app.get('/printers', (req, res) => {
-        res.json(printers);
-    });
-    app.get('/printers/:id', (req, res) => {
-        const id = parseInt(req.params.id);
-        const printer = printers.find(printer => printer.id === id);
-        if (printer) {
-            res.json(printer);
+
+/*let printers = [
+    { id: 1, enabled: true, ribbon: 40, paper: 20 },
+    { id: 2, enabled: true, ribbon: 30, paper: 15 },
+    { id: 3, enabled: true, ribbon: 20, paper: 5 },
+    { id: 4, enabled: true, ribbon: 10, paper: 10 },
+]*/
+
+const iniFile = './print_count.ini';
+let printers = ini.parse(fs.readFileSync(iniFile, 'utf-8'));
+
+app.get('/printers', (req, res) => {
+    res.json(printers);
+});
+app.get('/printers/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const printer = printers["PRINTER"+req.params.id] //printers.find(printer => printer.id === id);
+    if (printer) {
+        res.json(printer);
+    } else {
+        res.status(404).send('Item not found');
+    }
+});
+
+app.put('/printers/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const updatedPrinter = req.body;
+    const printer = printers["PRINTER"+req.params.id] //printers.findIndex(printer => printer.id === id);
+    console.log(updatedPrinter)
+    console.log(printer)
+    if (printer !== null) {
+        //printers[index] = { ...printers[index], ...updatedPrinter, id: id }; // Ensure ID remains unchanged
+        if(updatedPrinter.ribbon > 0 && updatedPrinter.paper > 0) {
+            printer.enabled = true;
         } else {
-            res.status(404).send('Item not found');
+            printer.enabled = false;
         }
-    });
+        printer.ribbon = updatedPrinter.ribbon;
+        printer.paper = updatedPrinter.paper;
 
-    app.put('/printers/:id', (req, res) => {
-        const id = parseInt(req.params.id);
-        const updatedPrinter = req.body;
-        const index = printers.findIndex(printer => printer.id === id);
-        if (index !== -1) {
-            printers[index] = { ...printers[index], ...updatedPrinter, id: id }; // Ensure ID remains unchanged
-            res.json(printers[index]);
-        } else {
-            res.status(404).send('Printer not found');
-        }
-    });
+        const iniText = ini.stringify(printers);
+        console.log(iniText);
+        fs.writeFileSync(iniFile, iniText);
 
-    app.get('/', function(req, res) {
-        res.sendFile(__dirname + '/index.html');
-    });
+        res.json(printer);
+    } else {
+        res.status(404).send('Printer not found');
+    }
+});
 
-    app.listen(port, () => {
-        console.log(`Server listening at http://localhost:${port}`);
-    });
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
